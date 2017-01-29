4e44d7daaacb34589a11c8b96b16d29532ab9f5d
Dodd <291106637@qq.com>
2017-01-29 19:09:29 +0800

    学习饿了么前端组件库element-ui，但感觉不是很好。

---
+++ node/element-ui-demo/src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24"><topNavBar></topNavBar></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4"><sideNavBar></sideNavBar></el-col>
+      <el-col :span="20">
+    <img src="./assets/logo.png">
+    <h1 class="happy-new-year">{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+  </el-col>
+  </el-row>
+</template>
+
+<script>
+import topNavBar from './topNavBar.vue'
+import sideNavBar from './sideNavBar.vue'
+export default {
+  name: 'app',
+  components: {
+    'topNavBar': topNavBar,
+    'sideNavBar':sideNavBar
+  },
+  data () {
+    return {
+      msg: '新年快乐'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 这里用scoped限定只在这个vue文件生效 */
+.happy-new-year{
+  color: red;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+ .el-row {
+    margin-bottom: 5px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
