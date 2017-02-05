b355615d2161d9db816583fc9efd291cfe5fab2c
Dodd <291106637@qq.com>
2017-02-05 13:14:24 +0800

    学习sequelizejs批量插入数据库

--- node/express/dao/initMysql.js
+++ node/express/dao/initMysql.js
@@ -1,10 +1,10 @@
 // 初始化数据库创建表
 var dao=require('./dao.js');
 
-var permission=dao.permission.permission;
+/*var permission=dao.permission.permission;
 permission.sync();
-var source=dao.permission.source;
-source.sync();
+var source=dao.permission.source; 资源和权限都在initPermission里
+source.sync();*/
 var user=dao.user
 user.sync();
 var role=dao.permission.role;
--- node/express/dao/initPermission.js
+++ node/express/dao/initPermission.js
@@ -1,1 +1,12 @@
-var dao=require('./dao.js');
+var dao = require('./dao.js');
+var permission = dao.permission.permission;
+var source = dao.permission.source;
+Promise.all(
+    [
+    	source.sync({ force: true }),
+        permission.sync({ force: true })
+    ]).then(function() {
+    	return source.bulkCreate(require('./PermissionSource.json'));
+}).then(function() {
+	// return permission.bulkCreate(require());
+});
