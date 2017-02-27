cd15b701b247a197eb857caeb89f8c70945344f2
Dodd <291106637@qq.com>
2017-02-27 15:14:45 +0800

    1 Signed-off-by: Dodd <291106637@qq.com>

--- C/HDU/2015.cpp
+++ C/HDU/2015.cpp
@@ -3,16 +3,14 @@ int main(){
 	int n,m,t;
 	while(~scanf("%d%d",&n,&m)){
 		t=n/m;
-		int f=(m+m*m)/m;
+		int f=1+m;
 		for(int i=0;i<t;i++){
 			if(i!=0)printf(" ");
 			printf("%d",f+i*2*m);
 		}
 
 		if(n%m!=0){
-			int y=n%m;
-			int x=(t*m)*2+1+y;
-			printf(" %d",x);
+			printf(" %d",t*m*2+1+n%m);
 		}
 		puts("");
 	}
