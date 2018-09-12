3125e35e599f0e8da956b050173754e3e91c0d3d
Dodd <291106637@qq.com>
2018-09-12 23:13:32 +0800

    frist ac code

--- C/codeforces/1/a.cpp
+++ C/codeforces/1/a.cpp
@@ -3,6 +3,6 @@ int main()
 {
     int n, m, a;
     scanf("%d%d%d", &n, &m, &a);
-    printf("%d", int((n + 1 - a) / a) * int((m + 1 - a) / a));
+    printf("%I64d", (long long)((n - 1 + a) / a) * (long long)((m - 1 + a) / a));
     return 0;
 }
